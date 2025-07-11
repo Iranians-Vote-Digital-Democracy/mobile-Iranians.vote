a3bcce74493672b454392c9e8c6c06b9dc5e94f4
Bogdan Guba <guba.bogd@gmail.com>
2025-07-11 15:36:28 +0300

    redesing for not supported docs

--- src/pages/app/pages/document-scan/components/DocumentPreviewStep.tsx
+++ src/pages/app/pages/document-scan/components/DocumentPreviewStep.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native'
 
 import AppContainer from '@/pages/app/components/AppContainer'
 import { useDocumentScanContext } from '@/pages/app/pages/document-scan/ScanProvider'
-import { UiButton, UiCard, UiHorizontalDivider } from '@/ui'
+import { UiButton, UiCard, UiHorizontalDivider, UiIcon } from '@/ui'
 import { EID, EPassport } from '@/utils/e-document'
 
 export default function DocumentPreviewStep() {
@@ -38,8 +38,8 @@ export default function DocumentPreviewStep() {
               Object.keys(restDetails).map(key => {
                 return (
                   <View key={key} className='flex flex-row items-center justify-between gap-2'>
-                    <Text className='typography-body3 capitalize text-textPrimary'>{key}</Text>
-                    <Text className='typography-subtitle4 text-textPrimary'>
+                    <Text className='capitalize text-textPrimary typography-body3'>{key}</Text>
+                    <Text className='text-textPrimary typography-subtitle4'>
                       {restDetails?.[key as keyof typeof tempEDoc.personDetails]}
                     </Text>
                   </View>
@@ -47,29 +47,29 @@ export default function DocumentPreviewStep() {
               })}
 
             <View className='flex flex-row items-center justify-between gap-2'>
-              <Text className='typography-body3 capitalize text-textPrimary'>dg1</Text>
-              <Text className='typography-subtitle4 text-textPrimary'>
+              <Text className='capitalize text-textPrimary typography-body3'>dg1</Text>
+              <Text className='text-textPrimary typography-subtitle4'>
                 {tempEDoc.dg1Bytes.length} length
               </Text>
             </View>
 
             <View className='flex flex-row items-center justify-between gap-2'>
-              <Text className='typography-body3 capitalize text-textPrimary'>dg11</Text>
-              <Text className='typography-subtitle4 text-textPrimary'>
+              <Text className='capitalize text-textPrimary typography-body3'>dg11</Text>
+              <Text className='text-textPrimary typography-subtitle4'>
                 {tempEDoc.dg11Bytes?.length} length
               </Text>
             </View>
 
             <View className='flex flex-row items-center justify-between gap-2'>
-              <Text className='typography-body3 capitalize text-textPrimary'>dg15</Text>
-              <Text className='typography-subtitle4 text-textPrimary'>
+              <Text className='capitalize text-textPrimary typography-body3'>dg15</Text>
+              <Text className='text-textPrimary typography-subtitle4'>
                 {tempEDoc.dg15Bytes?.length ?? 0} length
               </Text>
             </View>
 
             <View className='flex flex-row items-center justify-between gap-2'>
-              <Text className='typography-body3 capitalize text-textPrimary'>signature</Text>
-              <Text className='typography-subtitle4 text-textPrimary'>
+              <Text className='capitalize text-textPrimary typography-body3'>signature</Text>
+              <Text className='text-textPrimary typography-subtitle4'>
                 {tempEDoc?.aaSignature?.length ?? 0} length
               </Text>
             </View>
@@ -88,7 +88,7 @@ export default function DocumentPreviewStep() {
     return (
       <AppContainer className='flex-1 items-center justify-center'>
         <Text className='typography-title1 text-textPrimary'>EID Document Preview</Text>
-        <Text className='typography-body2 mt-2 text-textPrimary'>
+        <Text className='mt-2 text-textPrimary typography-body2'>
           EID document preview is not implemented yet.
         </Text>
       </AppContainer>
@@ -96,11 +96,24 @@ export default function DocumentPreviewStep() {
   }
 
   return (
-    <AppContainer className='flex-1 items-center justify-center'>
-      <Text className='typography-title1 text-textPrimary'>Document Preview</Text>
-      <Text className='typography-body2 mt-2 text-textPrimary'>
-        Document preview is not available for this document type.
+    <AppContainer className='mb-20 mt-10 flex-1 items-center justify-center p-6'>
+      <Text className='mb-2 text-textPrimary typography-h5'>Information</Text>
+      <Text className='mb-8 text-textSecondary typography-body3'>
+        Data generates a digital profile
       </Text>
+      <UiIcon
+        className='mb-8 mt-auto self-center color-textPrimary'
+        customIcon='userCircleIcon'
+        size={250}
+      />
+      <Text className='max-w-sm text-center text-textSecondary typography-body2'>
+        Document preview is not currently unavailable for this document type. We are actively
+        working on adding support!
+      </Text>
+      <View className='mt-auto'>
+        <UiHorizontalDivider className='my-5' />
+        <UiButton title='Generate Proof' onPress={createIdentity} />
+      </View>
     </AppContainer>
   )
 }
