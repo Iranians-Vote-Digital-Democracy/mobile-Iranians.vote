4b24e20880ea47bb94bdda84ee86ca2c3ec1e762
Bogdan Guba <guba.bogd@gmail.com>
2025-07-31 15:57:00 +0300

    Add password indicator to lockscreen

--- src/pages/local-auth/components/HiddenPasscodeView.tsx
+++ src/pages/local-auth/components/HiddenPasscodeView.tsx
@@ -1,15 +1,23 @@
 import type { ViewProps } from 'react-native'
 import { View } from 'react-native'
 
+import { cn } from '@/theme/utils'
+
 type Props = {
   length: number
 } & ViewProps
 
 export default function HiddenPasscodeView({ length }: Props) {
   return (
-    <View className='flex h-[16] flex-row items-center gap-2'>
-      {Array.from({ length }).map((_, i) => (
-        <View key={i} className='size-[16] rounded-full bg-primaryMain' />
+    <View className='flex h-[16] flex-row items-center gap-4'>
+      {Array.from({ length: 4 }).map((_, i) => (
+        <View
+          key={i}
+          className={cn(
+            'size-[16] rounded-full',
+            i < length ? 'bg-primaryMain' : 'bg-textSecondary',
+          )}
+        />
       ))}
     </View>
   )
