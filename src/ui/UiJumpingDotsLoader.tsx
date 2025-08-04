01a0848778165f572057f11622be5d5f01bafded
Bogdan Guba <guba.bogd@gmail.com>
2025-08-04 13:31:32 +0300

    Rewrite jumping dots use react-reanimated

--- src/ui/UiJumpingDotsLoader.tsx
+++ src/ui/UiJumpingDotsLoader.tsx
@@ -1,71 +1,71 @@
-import { useEffect, useRef } from 'react'
-import { Animated, Easing, View } from 'react-native'
+import { useEffect } from 'react'
+import { View } from 'react-native'
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withRepeat,
+  withSequence,
+  withTiming,
+} from 'react-native-reanimated'
 
 import { cn } from '@/theme'
 
 const UiJumpingDotsLoader = ({ size = 10, color = 'textPrimary' }) => {
-  const dot1Animation = useRef(new Animated.Value(0)).current
-  const dot2Animation = useRef(new Animated.Value(0)).current
-  const dot3Animation = useRef(new Animated.Value(0)).current
+  const dot1Y = useSharedValue(0)
+  const dot2Y = useSharedValue(0)
+  const dot3Y = useSharedValue(0)
 
   useEffect(() => {
-    const singleDotAnimation = animation =>
-      Animated.sequence([
-        Animated.timing(animation, {
-          toValue: -10,
-          duration: 300,
-          easing: Easing.out(Easing.ease),
-          useNativeDriver: true,
-        }),
-        Animated.timing(animation, {
-          toValue: 0,
-          duration: 300,
-          easing: Easing.in(Easing.ease),
-          useNativeDriver: true,
-        }),
-      ])
+    const jumpHeight = -10
+    const animationDuration = 400
+    const staggerDelay = 150
 
-    const staggeredAnimation = Animated.loop(
-      Animated.sequence([
-        singleDotAnimation(dot1Animation),
-        Animated.delay(150),
-        singleDotAnimation(dot2Animation),
-        Animated.delay(150),
-        singleDotAnimation(dot3Animation),
-        Animated.delay(150),
-      ]),
+    const jumpAnimation = withRepeat(
+      withSequence(
+        withTiming(jumpHeight, {
+          duration: animationDuration,
+          easing: Easing.out(Easing.ease),
+        }),
+        withTiming(0, {
+          duration: animationDuration,
+          easing: Easing.in(Easing.ease),
+        }),
+      ),
+      -1,
     )
 
-    staggeredAnimation.start()
+    dot1Y.value = withDelay(0, jumpAnimation)
+    dot2Y.value = withDelay(staggerDelay, jumpAnimation)
+    dot3Y.value = withDelay(staggerDelay * 2, jumpAnimation)
+  }, [dot1Y, dot2Y, dot3Y])
 
-    return () => staggeredAnimation.stop()
-  }, [dot1Animation, dot2Animation, dot3Animation])
+  const animatedDot1Style = useAnimatedStyle(() => ({
+    transform: [{ translateY: dot1Y.value }],
+  }))
+
+  const animatedDot2Style = useAnimatedStyle(() => ({
+    transform: [{ translateY: dot2Y.value }],
+  }))
+
+  const animatedDot3Style = useAnimatedStyle(() => ({
+    transform: [{ translateY: dot3Y.value }],
+  }))
 
   return (
     <View className='flex-row items-center justify-center gap-2'>
       <Animated.View
         className={cn('rounded-full', `bg-${color}`)}
-        style={{
-          width: size,
-          height: size,
-          transform: [{ translateY: dot1Animation }],
-        }}
+        style={[{ width: size, height: size }, animatedDot1Style]}
       />
       <Animated.View
         className={cn('rounded-full', `bg-${color}`)}
-        style={{
-          width: size,
-          height: size,
-          transform: [{ translateY: dot2Animation }],
-        }}
+        style={[{ width: size, height: size }, animatedDot2Style]}
       />
       <Animated.View
         className={cn('rounded-full', `bg-${color}`)}
-        style={{
-          width: size,
-          height: size,
-          transform: [{ translateY: dot3Animation }],
-        }}
+        style={[{ width: size, height: size }, animatedDot3Style]}
       />
     </View>
   )
